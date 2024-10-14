636188214e9ddafa482d4f7344f3edeedf1d0720
Amein100 <ndaroamin@gmail.com>
2024-10-14 12:18:43 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ownernomer = "254107163828"
+global.ownername = "Muhran"
+global.ytname = "YT: Muhran"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "Africa, Kenya, Nairobi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '254107163828'  //creator number
+global.ownername = 'Muhran' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
